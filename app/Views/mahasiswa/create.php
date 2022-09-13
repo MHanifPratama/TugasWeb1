d0e9acba1659d76b11e98e3c8a012fdbb4b5d5cc
MHanifPratama <mhanifp27@gmail.com>
2022-09-13 23:03:40 +0700

    KetigaX

---
+++ app/Views/mahasiswa/create.php
@@ -0,0 +1,15 @@
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">NPM</label>
+    <input type="text" class="form-control" name="npm" id="npm">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Nama</label>
+    <input type="text" class="form-control" name="nama" id="nama">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Alamat</label>
+    <input type="text" class="form-control" id="alamat" name="alamat">
+  </div>
+  <button type="submit" class="btn btn-primary">Tambah Data</button>
+</form>
