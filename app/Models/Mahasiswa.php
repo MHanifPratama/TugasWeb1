3e320c421b5f3ce2537a2ea18ddcafeb71c13e7b
MHanifPratama <mhanifp27@gmail.com>
2022-09-20 17:21:30 +0700

    KeempatS

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -14,7 +14,7 @@ class Mahasiswa extends Model
     // protected $returnType       = 'array';
     // protected $useSoftDeletes   = false;
     // protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['npm','nama','alamat'];
 
     // Dates
     protected $useTimestamps = true;
--- app/Views/mahasiswa/create.php
+++ app/Views/mahasiswa/create.php
@@ -1,15 +1,17 @@
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">NPM</label>
-    <input type="text" class="form-control" name="npm" id="npm">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Nama</label>
-    <input type="text" class="form-control" name="nama" id="nama">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Alamat</label>
-    <input type="text" class="form-control" id="alamat" name="alamat">
-  </div>
-  <button type="submit" class="btn btn-primary">Tambah Data</button>
-</form>
+<div class="p-4">
+    <form action="/store" method="post">
+        <div class="form-group">
+            <label for="npm">NPM</label>
+            <input type="text" name="npm" class="form-control" id="npm">
+        </div>
+        <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" name="nama" class="form-control" id="nama">
+        </div>
+        <div class="form-group">
+            <label for="alamat">Alamat</label>
+            <input type="text" name="alamat" class="form-control" id="alamat">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
